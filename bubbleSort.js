a8bb78521b9488b79712c4d3a822d55fb64530d3
smogecho <1982353566@qq.com>
2021-11-23 23:32:55 +0800

    bubble sort #!

---
+++ bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(list){
+  let flag = true;
+  for(let i=1; i< list.length && flag; i++){
+    flag = false;
+    for(let j =list.length-1; j>=i; j--){
+      let temp;
+      if(list[j] > list[j+1]){
+        temp = list[j];
+        list[j] = list[j+1];
+        list[j+1] = temp;
+        flag = true;
+      }
+    }
+  }
+  return list;
+}
